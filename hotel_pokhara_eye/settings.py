86b986e90f2d74df709ffb81d03fa30d54cb0108
shishirbhandari <shishirbhandari128@gmail.com>
2026-09-22 16:39:06 +0545

    feat: add initial Django settings configuration for hotel pokhara eye project

--- hotel_pokhara_eye/settings.py
+++ hotel_pokhara_eye/settings.py
@@ -155,7 +155,7 @@ EMAIL_BACKEND = os.environ.get(
     'EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend'
 )
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT') or 587)
 EMAIL_USE_TLS = env_bool('EMAIL_USE_TLS', True)
 EMAIL_USE_SSL = env_bool('EMAIL_USE_SSL', False)
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
